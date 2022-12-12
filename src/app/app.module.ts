e8e6d58c0ef697562364d97988bc47cb5f9e3a2d
Dioonel <dionelmartinezc@gmail.com>
2022-12-12 18:12:34 -0300

    Angular PWA installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 import { InterceptorService } from './services/interceptor.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -31,6 +33,18 @@ import { InterceptorService } from './services/interceptor.service';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatSelectCountryModule.forRoot('en'),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     CookieService,
